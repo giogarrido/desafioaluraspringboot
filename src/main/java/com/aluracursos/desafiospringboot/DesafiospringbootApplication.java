91c1a79d8505bdf02b3bd1f3bd0926ec1aa5d9fb
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-10-16 20:15:55 -0700

    first commit

---
+++ src/main/java/com/aluracursos/desafiospringboot/DesafiospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/desafiospringboot/DesafiospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
