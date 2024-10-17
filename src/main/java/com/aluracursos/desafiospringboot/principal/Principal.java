4e59efdefdd61b9a168e7bab7697c66844cb7cbf
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-10-17 09:45:38 -0700

    create models and funtions

--- src/main/java/com/aluracursos/desafiospringboot/principal/Principal.java
+++ src/main/java/com/aluracursos/desafiospringboot/principal/Principal.java
@@ -1,17 +1,61 @@
 package com.aluracursos.desafiospringboot.principal;
 
+import com.aluracursos.desafiospringboot.model.Datos;
+import com.aluracursos.desafiospringboot.model.DatosLibros;
 import com.aluracursos.desafiospringboot.service.ConsumoAPI;
 import com.aluracursos.desafiospringboot.service.ConvierteDatos;
 
 import java.sql.SQLOutput;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
+    private Scanner teclado = new Scanner(System.in);
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoAPI conusmoAPI = new ConsumoAPI();
-    private ConvierteDatos convierteDatos = new ConvierteDatos();
+    private ConvierteDatos conversor = new ConvierteDatos();
     
     public void muestraElMenu(){
         var json = conusmoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
-    }
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+        //top 10 libros mas descargados
+        System.out.println("Top 10 libros mas descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
+
+        //Busqueda de libros por nombre
+        System.out.println("Ingrese el nombre del libro que desea buscar");
+        var tituloLibro = teclado.nextLine();
+        json = conusmoAPI.obtenerDatos(URL_BASE + "?search=" + tituloLibro.replace(" ","+"));
+        var datosBusqueda = conversor.obtenerDatos(json,Datos.class);
+
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(l -> l.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("Libro Encontrado");
+            System.out.println(libroBuscado.get());
+        }else{
+            System.out.println("Lbro no encontrado");
+        }
+
+        //Trabajando con estadisticas
+        DoubleSummaryStatistics est = datos.resultados().stream()
+                .filter(d -> d.numeroDeDescargas() > 0)
+                .collect(Collectors.summarizingDouble(DatosLibros::numeroDeDescargas));
+        System.out.println("Cantidad media de descargas: " + est.getAverage());
+        System.out.println("Cantidad máxima de descargas: " + est.getMax());
+        System.out.println("Cantidad minima de descargas: " + est.getMin());
+        System.out.println("Cantidad de registros evaluados para calcular las estadisticas: " + est.getCount());
+
+    }//end muestra menu
 }
